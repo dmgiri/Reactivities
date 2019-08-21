c34ed095ee3107b7b97601f337a8398fa32ee95e
Niek Jan Ferwerda <niekjan.ferwerda@gmail.com>
2019-08-21 22:24:16 +0200

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ValuesController : ControllerBase
+  {
+    public DataContext context { get; }
+    public ValuesController(DataContext Context) { this.context = Context; }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Value>>> Get() => await context.Values.ToListAsync();
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Value>> Get(int id) { return await context.Values.FindAsync(id); }
+
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value) { }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value) { }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id) { }
+  }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration) { Configuration = configuration; }
+
+    public IConfiguration Configuration { get; }
+        
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<DataContext>(opt => { opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection")); });
+      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); } else {}  // app.UseHsts();
+      // app.UseHttpsRedirection();
+      app.UseMvc();
+    }
+  }
+}
